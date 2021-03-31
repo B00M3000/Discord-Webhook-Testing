dd100ec97f3ebc1de255e0545e5170ac7f14d83d
Thomas Zhou <doomking9000@gmail.com>
2021-03-31 01:48:17 +0000

    Finished createSchematic, editSchematic and deleteSchematic functions.

--- EventHandler.ts
+++ EventHandler.ts
@@ -6,10 +6,11 @@ interface Event {
 
 interface SchematicEvent extends Event{
   schematicId: string;
+  schematicName: string;
 }
 
 interface CreateSchematicEvent extends SchematicEvent{
-  
+
 }
 
 interface DeleteSchematicEvent extends SchematicEvent{
@@ -21,27 +22,38 @@ interface EditSchematicEvent extends SchematicEvent{
 }
 
 class EventHandler {
-  constructor(webhookHandler: DiscordWebhookHandler){
+  constructor(webhookHandler: DiscordWebhookHandler; websiteURL: string){
     this.webhookHandler = webhookHandler
+    this.websiteURL = websiteURL
   }
   
-  readonly webhookHandler: DiscordWebhookHandler
-  
-  readonly events: Event
+  webhookHandler: DiscordWebhookHandler
+  websiteURL: string
+  readonly events: Event[]
   
   createSchematic(event: CreateSchematicEvent){
-    webhookHandler.send({
-      embeds: [
-        
-      ]
+    webhookHandler.sendEmbed({
+      color: "green",
+      title: `New Schematic: ${event.schematicName}`,
+      url: `${this.websiteURL}/schematics/${event.schematicId}`
     })
   }
   
   editSchematic(event: EditSchematicEvent){
-    
+    webhookHandler.sendEmbed({
+      color: "yellow",
+      title: `Changed: ${event.schematicName}`,
+      description: event.changes,
+      url: `${this.websiteURL}/schematics/${event.schematicId}`
+    })
   }
   
   deleteSchematic(event: DeleteSchematicEvent){
-    
+    webhookHandler.sendEmbed({
+      color: "red",
+      title: `Deleted: ${event.schematicName}`,
+      description: event.reason,
+      url: `${this.websiteURL}/schematics/${event.schematicId}`
+    })
   }
 }
